ee31c9fecd268cfa4549a6bce3dff5ce6a05e9f9
hollarves <henryollh205@gmail.com>
2017-03-01 19:48:22 +0100

    Handling empty request body

--- csv-service/service.js
+++ csv-service/service.js
@@ -9,12 +9,15 @@ var express = require('express'),
     CsvService = express.Router()
 
 CsvService.post('/orders', basicAuthMiddleware, textParser, (req, res) => {
-    if (!req.body || req.body == {}) {
+    console.log("Body = ", Object.keys(req.body).length);
+    if (!req.body || Object.keys(req.body).length === 0) {
+        console.log('Here!');
         res.json({
             status: 400,
             message: "The body is empty"
         })
     } else {
+        console.log('here! shit')
         csvConverter({
                 noheader: false,
                 headers: ['tracking_number', 'amount', 'user_id', 'status']
